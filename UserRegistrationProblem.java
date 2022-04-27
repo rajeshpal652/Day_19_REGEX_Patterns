114645f2e1c0485daea291ae59308f0a8473c14e
rajeshpal652 <rajesh.pal.652@.com>
2022-04-27 19:54:08 +0530

    Added: User Registration Problem : Checking validity of Phone Number

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -1,24 +1,24 @@
 package com.bl.userregistration;
 /*
  * @Author: Rajesh Pal
- * Checking if entered name by use is valid or invalid.
+ * Checking if entered Phone number by user is valid or invalid.
  */
 import java.util.Scanner;
 
 public class UserRegistrationProblem {
 	public static void main(String[] args) {
-		System.out.println("Enter your Email Id : ");
+		System.out.println("Enter your Phone Number : ");
 		Scanner sc = new Scanner(System.in);
-		boolean result = eMail(sc.nextLine());
+		boolean result = phoneNumber(sc.nextLine());
 		
 		if(result) {
-			System.out.println("Email Id is Valid.");
+			System.out.println("Phonne Number is Valid.");
 		}else {
-			System.out.println("Email Id is Invalid.");
+			System.out.println("Phone Number is Invalid.");
 		}
 	}
 	
-	public static boolean eMail(String email) {
-		return email.matches("[a-zA-Z0-9]+[.][a-zA-Z0-9]*@([a-zA-Z]{2,})+([.][a-zA-Z]+)+");
+	public static boolean phoneNumber(String phoneNumber) {
+		return phoneNumber.matches("^[0-9]{2}\s[0-9]{10}");
 	}
 }
